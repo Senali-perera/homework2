60159ea8925918e4ad3aff45e4c3044acc6cf6a6
Senali-perera <senaliperera1996@gmail.com>
2024-01-13 19:04:05 +0200

    Added the second screen to add new todo and implement required functionalities there

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:homework2/todo.dart';
+import 'package:homework2/todo_list.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,58 +16,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: title,
       home: TodoList(),
-    );
-  }
-}
-
-class TodoList extends StatefulWidget {
-  @override
-  _TodoListState createState()=>_TodoListState();
-}
-
-class _TodoListState extends State<TodoList>{
-  final List<Todo> _todos = [];
-
-  void _addTodoItem(){
-    if (_todos.isEmpty){
-      for (var i = 0; i <= 100; i++) {
-        Todo newToDo = Todo("Buy Groceries $i" , false, "description");
-        _todos.add(newToDo);
-      }
-    }
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _addTodoItem();
-  }
-
-  Widget _viewTodoItem(Todo todo){
-    return ListTile(
-      title: Text(todo.title),
-      subtitle: Text(todo.description),
-      trailing: IconButton(
-        icon: const Icon(Icons.delete),
-        onPressed: () {
-          setState(() {
-            _todos.remove(todo);
-          });
-        },
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Todo List')),
-      body: ListView.builder(
-        itemCount: _todos.length,
-        itemBuilder: (context, index) {
-          return _viewTodoItem(_todos[index]);
-        },
-      ),
     );
   }
 }
